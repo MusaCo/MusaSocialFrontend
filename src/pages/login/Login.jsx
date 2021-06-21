bc4b0d98334812d24eaab3be0e1196d40c9d10f8
MusaCo <muslimxbox2@gmail.com>
2021-06-21 17:46:26 +0300

    added somethinggggggggg

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
         <div className="login">
             <div className="loginWrapper">
                 <div className="loginLeft">
-                    <h3 className="loginLogo">MusaSocial</h3>
+                    <h3 className="loginLogo">A7a Swk Social</h3>
                     <span className="loginDesc">Connect with Friends That You Dont Have :)</span>
                 </div>
                 <div className="loginRight">
